d3affc4b56094e0c4382350db9fa319a9e0e3a1c
Rubén <rubensalinasgarcia@pm.me>
2020-05-03 19:56:12 +0200

    Changed status bar

--- emacs.d/elpa/org-bullets-20180208.2343/org-bullets-autoloads.el
+++
@@ -1,41 +0,0 @@
-;;; org-bullets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "org-bullets" "../../../../../.emacs.d/elpa/org-bullets-20180208.2343/org-bullets.el"
-;;;;;;  "5b300cbdffe13e87d5db13249aa4d003")
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/org-bullets-20180208.2343/org-bullets.el
-
-(autoload 'org-bullets-mode "org-bullets" "\
-Use UTF8 bullets in Org mode headings.
-
-\(fn &optional ARG)" t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "org-bullets"
-;;;;;;  "../../../../../.emacs.d/elpa/org-bullets-20180208.2343/org-bullets.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/org-bullets-20180208.2343/org-bullets.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "org-bullets" '("org-bullets-")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/org-bullets-20180208.2343/org-bullets-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/org-bullets-20180208.2343/org-bullets.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; org-bullets-autoloads.el ends here

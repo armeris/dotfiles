ebac1777591ab03e86615b9e591cfa766dea6499
Rubén Salinas <ruben.salinas@edreamsodigeo.com>
2021-09-15 07:12:23 +0200

    Updated config

--- lock.sh
+++ lock.sh
@@ -8,32 +8,32 @@ W='#880000bb'  # wrong
 V='#bb00bbbb'  # verifying
 
 i3lock \
---insidevercolor=$C   \
---ringvercolor=$V     \
+--insidever-color=$C   \
+--ringver-color=$V     \
 \
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
+--insidewrong-color=$C \
+--ringwrong-color=$W   \
 \
---insidecolor=$B      \
---ringcolor=$D        \
---linecolor=$B        \
---separatorcolor=$D   \
+--inside-color=$B      \
+--ring-color=$D        \
+--line-color=$B        \
+--separator-color=$D   \
 \
---verifcolor=$T        \
---wrongcolor=$T        \
---timecolor=$T        \
---datecolor=$T        \
---layoutcolor=$T      \
---keyhlcolor=$W       \
---bshlcolor=$W        \
+--verif-color=$T        \
+--wrong-color=$T        \
+--time-color=$T        \
+--date-color=$T        \
+--layout-color=$T      \
+--keyhl-color=$W       \
+--bshl-color=$W        \
 \
---screen 1            \
---blur 5              \
+--screen=0            \
+--blur=5              \
 --clock               \
 --indicator           \
---timestr="%H:%M:%S"  \
---datestr="%A, %m %Y" \
---keylayout 2         \
+--time-str="%H:%M:%S"  \
+--date-str="%A, %m %Y" \
+--keylayout=2         \
 
 # --veriftext="Drinking verification can..."
 # --wrongtext="Nope!"
